297137de4a1db9b0fb4abcbb718909fb6932f10e
Eric Simpson <eric.gr.simpson@gmail.com>
2025-09-03 23:03:55 -0400

    allow subtraction mode for adders

--- src/structural/adder/adder.v
+++ src/structural/adder/adder.v
@@ -5,13 +5,23 @@ module adder(x, y, c_in, s, c_out);
     output [7:0] s;
     output c_out;
     wire [6:0] c;
+    wire [7:0] ny;
 
-    fulladd fa0(c_in, x[0], y[0], s[0], c[0]);
-    fulladd fa1(c[0], x[1], y[1], s[1], c[1]);
-    fulladd fa2(c[1], x[2], y[2], s[2], c[2]);
-    fulladd fa3(c[2], x[3], y[3], s[3], c[3]);
-    fulladd fa4(c[3], x[4], y[4], s[4], c[4]);
-    fulladd fa5(c[4], x[5], y[5], s[5], c[5]);
-    fulladd fa6(c[5], x[6], y[6], s[6], c[6]);
-    fulladd fa7(c[6], x[7], y[7], s[7], c_out);
+    xor y0(ny[0], y[0], c_in);
+    xor y1(ny[1], y[1], c_in);
+    xor y2(ny[2], y[2], c_in);
+    xor y3(ny[3], y[3], c_in);
+    xor y4(ny[4], y[4], c_in);
+    xor y5(ny[5], y[5], c_in);
+    xor y6(ny[6], y[6], c_in);
+    xor y7(ny[7], y[7], c_in);
+
+    fulladd fa0(c_in, x[0], ny[0], s[0], c[0]);
+    fulladd fa1(c[0], x[1], ny[1], s[1], c[1]);
+    fulladd fa2(c[1], x[2], ny[2], s[2], c[2]);
+    fulladd fa3(c[2], x[3], ny[3], s[3], c[3]);
+    fulladd fa4(c[3], x[4], ny[4], s[4], c[4]);
+    fulladd fa5(c[4], x[5], ny[5], s[5], c[5]);
+    fulladd fa6(c[5], x[6], ny[6], s[6], c[6]);
+    fulladd fa7(c[6], x[7], ny[7], s[7], c_out);
 endmodule
--- src/structural/adder/adder_tb.v
+++ src/structural/adder/adder_tb.v
@@ -3,23 +3,32 @@ module adder_tb;
     // Declare testbench signals
     reg [7:0] a;
     reg [7:0] b;
+    reg c_in;
     wire [7:0] s;
     wire c;
 
     adder add (
         .x(a),
         .y(b),
-        .c_in(1'b0),
+        .c_in(c_in),
         .s(s),
         .c_out(c)
     );
 
     initial begin
-        // Apply test vectors
+        // Apply test vectors 15 + 3 = 18 (8'b00010010)
         a = 8'b00001111;
         b = 8'b00000011;
+        c_in = 1'b0; // addition
         #10; // Wait for 10 time units
-        $display("c_in=%b, a=%b, b=%b, s=%b, c_out=%b", 1'b0, a, b, s, c); 
+        $display("c_in=%b, a=%b, b=%b, s=%b, c_out=%b", c_in, a, b, s, c); 
+
+        // Apply test vectors 15 - 3 = 12 (8'b00001100)
+        a = 8'b00001111;
+        b = 8'b00000011;
+        c_in = 1'b1; // subtraction
+        #10; // Wait for 10 time units
+        $display("c_in=%b, a=%b, b=%b, s=%b, c_out=%b", c_in, a, b, s, c); 
 
         // End the simulation
         $finish;
